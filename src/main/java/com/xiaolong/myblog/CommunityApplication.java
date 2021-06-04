2e93c5c8838f10338abdb2f4b8b0bdcd91358043
[xiaolong] <[xiaolong@gmail.com]>
2021-06-04 15:15:47 +0800

    1.[提交] 第一次提交代码

--- src/main/java/com/xiaolong/myblog/CommunityApplication.java
+++ src/main/java/com/xiaolong/myblog/CommunityApplication.java
@@ -1,4 +1,4 @@
-package com.xiaolong.community;
+package com.xiaolong.myblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/xiaolong/myblog/controller/IndexController.java
+++ src/main/java/com/xiaolong/myblog/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.xiaolong.community.controller;
+package com.xiaolong.myblog.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
