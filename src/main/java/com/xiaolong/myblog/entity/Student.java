3d5442e1f4a3c3f6ff6186213d7211f407f95336
[xiaolong] <[xiaolong@gmail.com]>
2021-07-02 21:16:17 +0800

    1.[提交] 添加测试

---
+++ src/main/java/com/xiaolong/myblog/entity/Student.java
@@ -0,0 +1,35 @@
+package com.xiaolong.myblog.entity;
+
+/**
+ * 学生测试类
+ * @author dcl
+ * @since 6:06 下午 2021/6/15
+ */
+public class Student {
+
+    private String name;
+
+    private Integer age;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Student(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+}
