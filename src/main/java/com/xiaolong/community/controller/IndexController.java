799cff8adf2656a83688e6f3b322692d6a14d2fb
[xiaolong] <[xiaolong@gmail.com]>
2021-07-06 15:57:56 +0800

    [提交] 修改index接口

--- src/main/java/com/xiaolong/community/controller/IndexController.java
+++ src/main/java/com/xiaolong/community/controller/IndexController.java
@@ -1,9 +1,7 @@
 package com.xiaolong.community.controller;
 
-import com.xiaolong.community.service.IndexRetryService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import javax.annotation.Resource;
 
 /**
  * @author xiaolong1994
@@ -11,15 +9,12 @@ import javax.annotation.Resource;
 @RestController
 public class IndexController {
 
-    @Resource
-    private IndexRetryService indexRetryService;
-
     /**
      * 默认显示index界面
      */
     @GetMapping("/index")
     public String index(){
-        return "访问成功";
+        return "打印一行文字,显示XXXX";
     }
 
 }
